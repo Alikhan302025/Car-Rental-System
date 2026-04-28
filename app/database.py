250390232ef24e20f709ef709f0da5e6b6c1b3fd
alikhan zhambayev <254530@astanait.edu.kz>
2026-04-28 17:01:14 +0500

    move config to env and setup database connection

--- app/database.py
+++ app/database.py
@@ -1,9 +1,11 @@
 import asyncpg
+from config import DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT
 
 async def connect_db():
     return await asyncpg.connect(
-        user = "postgres",
-        password = "1234",
-        database = "car_rental",
-        host = "localhost"
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database=DB_NAME,
+        host=DB_HOST,
+        port=DB_PORT
     )
